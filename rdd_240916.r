07b3a95b8303d6350219514bbac4acd6dcb24721
yuchengwang-econ <wangyucheng1999@126.com>
2024-09-23 17:27:12 -0400

    update rdd
    add local regression with triangular kernel

--- rdd_240916.r
+++ rdd_240916.r
@@ -64,3 +64,57 @@ gg = ggplot(data = data.table(thresholds, results), aes(x=thresholds, y=results)
   geom_hline(yintercept = 10.0, linetype="dashed", color="red")
 
 ggsave(gg, file="RDD_estimator_simple.pdf", width=8, height=5)
+
+
+###################################
+## RDD estimator with triangular kernel
+###################################
+
+rm(list=ls())   # clear memory
+setwd("~/github/Econ512")
+
+## load the data
+data_set = read.csv("RDD_simulator_data.csv")
+data_set = setDT(data_set)
+
+RDD_estimator_triangular <- function(inputdata, c_threshold_point=0, tau_bandwith=0.5){
+  inputdata[, normalized_distance := (X_running_variable - c_threshold_point) / tau_bandwith]
+  inputdata[, kernel_weight := (1 - abs(normalized_distance))*(abs(normalized_distance <= 1))]
+  
+  triangular_loss_function <- function(params){
+    alpha_guess = params[1]
+    gamma_guess = params[2]
+    
+    ## Calculate the loss function value
+    inputdata_subset[, objective_part <- (Y_outcome_of_interest - alpha_guess - gamma_guess * (X_running_variable - c_threshold_point))^2]
+    objective_value = inputdata_subset[, sum(objective_value * kernel_weight)]
+    return(objective_value)
+  }
+  
+  ## Minimize the loss function, separately for treated and control groups
+  optim_result_treated = optim(c(0,0), triangular_loss_function, inputdata_subset = inputdata[D_treatment==TRUE])
+  optim_result_control = optim(c(0,0), triangular_loss_function, inputdata_subset = inputdata[D_treatment==FALSE])
+  
+  ## Extract the treatment effect
+  alpha_treated = optim_result_treated$par[1]
+  alpha_control = optim_result_control$par[1]
+  RDD_triangular = alpha_treated - alpha_control
+  return(RDD_triangular)
+}
+
+## Adjust bandwidth tau
+thresholds = seq(0.1, 1.0, by=0.1)
+results = rep(0, length(thresholds))
+for(i in 1:length(thresholds)){
+  results[i] = RDD_estimator_triangular(data_set, c_threshold_point = 0, tau_bandwith = thresholds[i])
+}
+
+gg = ggplot(data = data.table(thresholds, results), aes(x=thresholds, y=results)) +
+  geom_point() +
+  geom_line() +
+  theme_bw(base_size=16) +
+  labs(x="Bandwith (tau)", y="Treatment Effect Estimate") +
+  geom_hline(yintercept = 10.0, linetype="dashed", color="red")
+
+ggsave(gg, file="RDD_estimator_triangular.pdf", width=8, height=5)
+
